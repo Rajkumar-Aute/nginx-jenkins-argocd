aa5561d7fb8f35b7c2de2d9800941235953cbffa
Rajkumar Aute <rajkumaraute@gmail.com>
2023-04-11 15:56:20 +0530

    script-download

--- script/updateyaml.sh
+++ script/updateyaml.sh
@@ -5,5 +5,5 @@ cat deployment.yaml
 sed -i 's+$DOCKERHUB_URL.*+$DOCKERHUB_URL:$BUILD_NUMBER+g' deployment.yaml
 cat deployment.yaml
 git add .
-git commit -m 'Done by Jenkins Job update manifest: ${env.BUILD_NUMBER}'
+git commit -m 'Done by Jenkins Job update manifest: ${env.BUILD_NUMBER}' && \
 git push --force https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GIT_USERNAME}/nginx-jenkins-argocd.git HEAD:main
